a9689329622c8a8715687f9b94c85678ee5cf8b3
Vasudev290 <vasukesavulu@gmail.com>
2025-04-30 18:36:39 +0530

    style

--- Chat-GPT-Clone/client/src/Components/Navbar.jsx
+++ Chat-GPT-Clone/client/src/Components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
+
 const Navbar = () => {
   const theme = useTheme();
   const navigate = useNavigate();
--- Chat-GPT-Clone/client/src/pages/Homepage.jsx
+++ Chat-GPT-Clone/client/src/pages/Homepage.jsx
@@ -1,11 +1,191 @@
-import React from 'react'
-
+import React from "react";
+import { Box, Typography, Card, Stack } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import DescriptionRounded from "@mui/icons-material/DescriptionRounded";
+import FormatAlignLeftOutlined from "@mui/icons-material/FormatAlignLeftOutlined";
+import ChatRounded from "@mui/icons-material/ChatRounded";
 const Homepage = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-        <h1>Home Page</h1>
-    </div>
-  )
-}
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexWrap: "nowrap",
+          overflowX: "auto",
+          gap: 2,
+          px: 2,
+          py: 4,
+        }}
+      >
+        <Box p={2}>
+          <Typography variant="h4" mb={2} fontWeight="bold">
+            Text Generation
+          </Typography>
+          <Card
+            onClick={() => navigate("/summary")}
+            sx={{
+              boxShadow: 2,
+              borderRadius: 5,
+              height: 190,
+              width: 200,
+              "&:hover": {
+                border: 2,
+                boxShadow: 0,
+                borderColor: "primary.dark",
+                cursor: "pointer",
+              },
+            }}
+          >
+            <DescriptionRounded
+              sx={{ fontSize: 20, color: "primary.main", mt: 2, ml: 2 }}
+            />
+            <Stack p={1} pt={0}>
+              <Typography fontWeight="bold" variant="h6">
+                TEXT SUMAMRY
+              </Typography>
+              <Typography variant="h6">
+                Summarize long text into short sentences
+              </Typography>
+            </Stack>
+          </Card>
+        </Box>
+        <Box p={2}>
+          <Typography variant="h4" mb={2} fontWeight="bold">
+            Parapgraph Generation
+          </Typography>
+          <Card
+            onClick={() => navigate("/paragraph")}
+            sx={{
+              boxShadow: 2,
+              borderRadius: 5,
+              height: 190,
+              width: 200,
+              "&:hover": {
+                border: 2,
+                boxShadow: 0,
+                borderColor: "primary.dark",
+                cursor: "pointer",
+              },
+            }}
+          >
+            <FormatAlignLeftOutlined
+              sx={{ fontSize: 20, color: "primary.main", mt: 2, ml: 2 }}
+            />
+            <Stack p={1} pt={0}>
+              <Typography fontWeight="bold" variant="h6">
+                Parapgraph
+              </Typography>
+              <Typography variant="h6">
+                Generate Paragraph with words
+              </Typography>
+            </Stack>
+          </Card>
+        </Box>
+        <Box display="flex" justifyContent="center">
+          <Box p={2}>
+            <Typography
+              variant="h4"
+              mb={2}
+              fontWeight="bold"
+              
+            >
+              AI ChatBot Coverted
+            </Typography>
+            <Card
+              onClick={() => navigate("/chatbot")}
+              sx={{
+                boxShadow: 2,
+                borderRadius: 5,
+                height: 190,
+                width: 200,
+                "&:hover": {
+                  border: 2,
+                  boxShadow: 0,
+                  borderColor: "primary.dark",
+                  cursor: "pointer",
+                },
+              }}
+            >
+              <ChatRounded
+                sx={{ fontSize: 20, color: "primary.main", mt: 2, ml: 2 }}
+              />
+              <Stack p={1} pt={0}>
+                <Typography fontWeight="bold" variant="h6">
+                  Chatbot
+                </Typography>
+                <Typography variant="h6">Chat With AI Chatbot</Typography>
+              </Stack>
+            </Card>
+          </Box>
+        </Box>
 
-export default Homepage
+        <Box p={2}>
+          <Typography variant="h4" mb={2} fontWeight="bold">
+            Javascript Converter
+          </Typography>
+          <Card
+            onClick={() => navigate("/js-converter")}
+            sx={{
+              boxShadow: 2,
+              borderRadius: 5,
+              height: 190,
+              width: 200,
+              "&:hover": {
+                border: 2,
+                boxShadow: 0,
+                borderColor: "primary.dark",
+                cursor: "pointer",
+              },
+            }}
+          >
+            <ChatRounded
+              sx={{ fontSize: 20, color: "primary.main", mt: 2, ml: 2 }}
+            />
+            <Stack p={1} pt={0}>
+              <Typography fontWeight="bold" variant="h6">
+                JS CONVERTER
+              </Typography>
+              <Typography variant="h6">
+                Trasnlate english to javascript code
+              </Typography>
+            </Stack>
+          </Card>
+        </Box>
+        <Box p={2}>
+          <Typography variant="h4" mb={2} fontWeight="bold">
+            AI SCIFI Images
+          </Typography>
+          <Card
+            onClick={() => navigate("/scifi-image")}
+            sx={{
+              boxShadow: 2,
+              borderRadius: 5,
+              height: 190,
+              width: 200,
+              "&:hover": {
+                border: 2,
+                boxShadow: 0,
+                borderColor: "primary.dark",
+                cursor: "pointer",
+              },
+            }}
+          >
+            <ChatRounded
+              sx={{ fontSize: 20, color: "primary.main", mt: 2, ml: 2 }}
+            />
+            <Stack p={1} pt={0}>
+              <Typography fontWeight="bold" variant="h6">
+                Scifi Image
+              </Typography>
+              <Typography variant="h6">Generate Scifi images</Typography>
+            </Stack>
+          </Card>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Homepage;
